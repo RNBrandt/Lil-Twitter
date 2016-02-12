c78272238b2bbf07d9dd5b5503ee5071baa7f636
David Rothschild <david@rothschildsolutions.com>
2016-02-11 17:36:08 -0800

    create migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,21 @@
 class User < ActiveRecord::Base
+  has_many: tweets
+  has_many: relationships, foreign_key: follower_id
+  has_many: followers, class_name: "User", through: relationships
+
+  # def relationships
+  #   Relationship.where(user_id: self.id)
+  # end
+
+  # method named relationships
+  # model called Relationship
+
   include BCrypt
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence:true, uniqueness: true
+  validates :handle, presence:true, uniqueness: true
   validates :password_hash, presence: true
 
   def password

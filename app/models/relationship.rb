30e08a98830ed6b54ada1dc4563a9030846059a8
David Rothschild <david@rothschildsolutions.com>
2016-02-11 18:21:16 -0800

    add model for users and tweets

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,3 +1,8 @@
 class Relationship < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
+  validates_uniquenes_of :follower_id, :scope => :followed_id
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,11 @@
 class User < ActiveRecord::Base
-  has_many: tweets
-  has_many: relationships, foreign_key: follower_id
-  has_many: followers, class_name: "User", through: relationships
-
-  # def relationships
-  #   Relationship.where(user_id: self.id)
-  # end
-
-  # method named relationships
-  # model called Relationship
 
   include BCrypt
+  has_many :tweets
+  has_many :relationships, foreign_key: "follower_id"
+  has_many :reverse_relationships, class_name: "Relationship", foreign_key:"followed_id"
+  has_many :following through: :relationships, source: :followed
+  has_many :followers through: :reverse_relationships, source: :follower
 
   validates :first_name, presence: true
   validates :last_name, presence: true
@@ -30,6 +25,18 @@ class User < ActiveRecord::Base
   def self.authenticate(email, password)
     user = User.find_by(email: email)
     return user if user && user.password == password
+  end
+
+  def follow(other_user)
+    relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
 end
